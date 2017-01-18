028c13df52a7466953c631ad5f6dbde605a67140
Saqib Naveed <saqibnaveed27@gmail.com>
2017-01-18 18:17:42 +0500

    Projects page added

--- server.js
+++ server.js
@@ -37,15 +37,19 @@ app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Hi, welcome to my website.',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Hi, welcome to my website.'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
   });
 });
 
